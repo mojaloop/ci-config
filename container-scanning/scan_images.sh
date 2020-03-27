714570a012157042f0a7fc15a4085bbf631e800f
Lewis Daly <lewisdaly@me.com>
2020-03-27 15:45:45 +0800

    update script

--- container-scanning/scan_images.sh
+++ container-scanning/scan_images.sh
@@ -8,6 +8,8 @@ set -o nounset
 ########################
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ANCHORE_URL="http://localhost:8228"
+INLINE_SCAN_IMAGE="${INLINE_SCAN_IMAGE:-docker.io/anchore/inline-scan:v0.6.1}"
+
 # ANCHORE_USER="admin"
 # ANCHORE_PASS="foobar"
 # TODO: allow for scanning multiple images?
@@ -21,44 +23,81 @@ POLICY_NAME="${POLICY_NAME:-default_policy}"
 WORKING_DIR="${WORKING_DIR:-/tmp/ci-config}"
 
 
-mkdir -p ${WORKING_DIR}
-cd ${WORKING_DIR}
-pwd
-mkdir -p ${RESULT_DIR}
-pwd
+
+main() {
+  mkdir -p ${WORKING_DIR}
+  cd ${WORKING_DIR}
+  mkdir -p ${RESULT_DIR}
+  
+  prepare_inline_container
+
+}
+
 
 ### Testing out as a replacement to the broken Anchore CLI Orb
 
 #TODO: check and install:
 # pip, docker-compose
 
-pip install --user anchorecli
+# pip install --user anchorecli
 
 
-# Start Anchore Engine with docker-compose
-docker pull docker.io/anchore/anchore-engine:latest
-docker create --name ae docker.io/anchore/anchore-engine:latest
-docker cp ae:/docker-compose.yaml ./docker-compose.yaml
-docker rm ae
-
-pwd
-
-docker-compose pull
-docker-compose up
-docker-compose ps
+# # Start Anchore Engine with docker-compose
+# docker pull docker.io/anchore/anchore-engine:latest
+# docker create --name ae docker.io/anchore/anchore-engine:latest
+# docker cp ae:/docker-compose.yaml ./docker-compose.yaml
+# docker rm ae
 
 
-## Configure Anchore Policies
-anchore-cli policy add ${POLICY_BUNDLE}
-anchore-cli policy activate ${POLICY_NAME}
+# Copied from anchore engine inline scanner
+prepare_inline_container() {
+    # Check if env var is overriding which inline-scan image to utilize.
+    # if [[ -z "${ANCHORE_CI_IMAGE}" ]]; then
+        # printf '\n%s\n' "Pulling ${INLINE_SCAN_IMAGE}"
+        docker pull "${INLINE_SCAN_IMAGE}"
+    # else
+    #     printf '\n%s\n' "Using local image for scanning -- ${INLINE_SCAN_IMAGE}"
+    # fi
 
-## Run the scan(s)
-anchore-cli image add ${IMAGE}
-anchore-cli image wait ${IMAGE}
-anchore-cli image list
-anchore-cli image get ${IMAGE}
-anchore-cli --json image vuln ${IMAGE} all > ${RESULT_DIR}${IMAGE//\//_}-vuln.json
-anchore-cli --json evaluate check ${IMAGE} --detail > ${RESULT_DIR}${IMAGE//\//_}-eval.json
+    # setup command arrays to eval & run after adding all required options
+    CREATE_CMD=('docker create --name "${DOCKER_NAME}"')
+    RUN_CMD=('docker run -i --name "${DOCKER_NAME}"')
 
-## Cleanup
-docker-compose rm -f
+    # if [[ "${t_flag}" ]]; then
+    #     CREATE_CMD+=('-e TIMEOUT="${TIMEOUT}"')
+    #     RUN_CMD+=('-e TIMEOUT="${TIMEOUT}"')
+    # fi
+    # if [[ "${V_flag}" ]]; then
+    #     CREATE_CMD+=('-e VERBOSE=true')
+    #     RUN_CMD+=('-e VERBOSE=true')
+    # fi
+
+    CREATE_CMD+=('"${INLINE_SCAN_IMAGE}"')
+    RUN_CMD+=('"${INLINE_SCAN_IMAGE}"')
+}
+
+
+# docker-compose pull
+# docker-compose up -d
+# # TODO: wait for docker-compose somehow?
+# docker-compose ps
+
+
+# ## Configure Anchore Policies
+# anchore-cli policy add ${POLICY_BUNDLE}
+# anchore-cli policy activate ${POLICY_NAME}
+
+# ## Run the scan(s)
+# anchore-cli image add ${IMAGE}
+# anchore-cli image wait ${IMAGE}
+# anchore-cli image list
+# anchore-cli image get ${IMAGE}
+# anchore-cli --json image vuln ${IMAGE} all > ${RESULT_DIR}${IMAGE//\//_}-vuln.json
+# anchore-cli --json evaluate check ${IMAGE} --detail > ${RESULT_DIR}${IMAGE//\//_}-eval.json
+
+# ## Cleanup
+# docker-compose rm -f
+
+
+
+main "$@"
